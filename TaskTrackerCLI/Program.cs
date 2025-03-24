28b107d82b70c2426c2d524d5e2aced2efebd456
TheSwimon <lordkipanidzegeorge@gmail.com>
2025-03-25 01:23:06 +0400

    Update Program.cs

--- TaskTrackerCLI/Program.cs
+++ TaskTrackerCLI/Program.cs
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-=======
+
 ﻿using System.Reflection;
 using System.Text.RegularExpressions;
 using TaskTrackerCLI.Services;
@@ -10,4 +7,3 @@ using TaskTrackerCLI.Services;
 CommandHandler commandHandler = new CommandHandler();
 
 commandHandler.ExecuteCommand(args);
->>>>>>> 6e80a55 (added help command and cleaned and cleaned the outputs)
